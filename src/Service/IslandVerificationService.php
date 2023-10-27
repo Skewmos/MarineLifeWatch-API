82ac10d0e27f44ba2c7af0ad05d433f15b305418
Legrand Jérémie <jlegrand@cxfirst.com>
2023-10-27 21:58:11 +0200

    fix(403): Changing http client using Guzzle

--- src/Service/IslandVerificationService.php
+++ src/Service/IslandVerificationService.php
@@ -3,35 +3,61 @@
 namespace App\Service;
 
 use AllowDynamicProperties;
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-#[AllowDynamicProperties] class IslandVerificationService
+#[AllowDynamicProperties]
+class IslandVerificationService
 {
     private string $apiBaseUrl;
 
-    public function __construct(string $apiBaseUrl, HttpClientInterface $httpClient)
+    public function __construct(string $apiBaseUrl)
     {
         $this->apiBaseUrl = $apiBaseUrl;
-        $this->httpClient = $httpClient;
     }
 
-    public function verifyIslandId(string $island, string $responseMode="json"): ?array
+
+    private function buildApiUrl(string $island, string $responseMode): string
+    {
+        return sprintf('%s/pandore/ilot/IlotDto/%s?_responseMode=%s', $this->apiBaseUrl, $island, $responseMode);
+    }
+
+    private function sendHttpRequest(string $url): ResponseInterface
+    {
+        $client = new Client();
+
+        $options = [
+            'headers' => [
+                'content-type' => 'application/json+ld',
+                'accept' => 'application/json+ld',
+            ],
+            'timeout' => 10,
+        ];
+
+        return $client->request('GET', $url, $options);
+    }
+
+    public function verifyIslandId(string $island, string $responseMode="json")
     {
         if (empty($island)) {
             throw new \Exception("The island identifier cannot be an empty string");
         }
 
         try {
-            $url = $this->apiBaseUrl . '/pandore/ilot/IlotDto/'. $island .'&_responseMode='.$responseMode;
-            $response = $this->httpClient->request('GET', $url);
-            if ($response->getStatusCode() === 200) {
-                return $response->toArray();
-            } else {
-                return null;
+            $url = $this->buildApiUrl($island, $responseMode);
+            $response = $this->sendHttpRequest($url);
+            $content = json_decode($response->getBody()->getContents(), true);
+            if ($response->getStatusCode() === 200 && isset($content['data'])) {
+                return $content['data'];
             }
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'HTTP error: ' . $e->getMessage()], $e->getStatusCode());
         } catch (TransportExceptionInterface $e) {
-            return ['error' => $e];
+            return new JsonResponse(['error' => 'Transport error: ' . $e->getMessage()], 500);
         }
+
+        return null;
     }
 }
